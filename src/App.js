b74e5c9797e249dd37d8c6a74ed8bc8b4d5565a1
tommy <tommy.zhang@chelion.com>
2023-03-21 13:56:03 +0800

    add

--- src/App.js
+++ src/App.js
@@ -1,24 +1,49 @@
-import logo from './logo.svg';
+import React, { useEffect } from 'react';
 import './App.css';
+import { neonCursor } from 'https://unpkg.com/threejs-toys@0.0.8/build/threejs-toys.module.cdn.min.js'
+
+import logo from './left.png';
+import right from './right.png';
+import top from './top.png';
+import rt from './rt.png';
+
 
 function App() {
+  useEffect(() => {
+    neonCursor({
+      el: document.getElementById('app'),
+      shaderPoints: 16,
+      curvePoints: 80,
+      curveLerp: 0.5,
+      radius1: 5,
+      radius2: 30,
+      velocityTreshold: 10,
+      sleepRadiusX: 100,
+      sleepRadiusY: 100,
+      sleepTimeCoefX: 0.0025,
+      sleepTimeCoefY: 0.0025
+    })
+   
+  }, []);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div id="app" style={{height:"100%"}}>
+
+      <div id="hero">
+        <h1>π<br />3.14</h1>
+        <a>Mint</a>
+      </div>
+      <div style={{position:'absolute',left:'15%',top:'25%',right:0,width:'50%',zIndex:5}}>
+        <img style={{width:150,borderRadius:'100px'}} src={rt}></img>
+      </div>
+      <div style={{position:'absolute',left:'25%',top:'25%',right:0,width:'50%',zIndex:5}}>
+        <img style={{width:150,borderRadius:'100px'}} src={logo}></img>
+      </div>
+      <div style={{position:'absolute',left:'35%',top:'25%',right:0,width:'50%',zIndex:5}}>
+        <img style={{width:150,borderRadius:'100px'}} src={right}></img>
+      </div>
     </div>
+
   );
 }
 
